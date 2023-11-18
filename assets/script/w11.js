7a92c0f8137d341f90beb142096fd4c46f70c67b
ksvaska <vas25ks@gmail.com>
2023-11-18 20:19:07 +0300

    first

---
+++ assets/script/w11.js
@@ -0,0 +1,9 @@
+const priceK = document.getElementById('price_k');
+const priseC = document.getElementById('price_c');
+const priceS = document.getElementById('price_s');
+const priceB = document.getElementById ('price_b');
+const totalPrice = document.getElementById('totalPrice');
+const btn = document.getElementById('btn');
+
+
+
