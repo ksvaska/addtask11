48d409b2c4b7c2e1fdde5ca3c38337aa32da0e71
ksvaska <vas25ks@gmail.com>
2023-11-21 15:56:35 +0300

    two

--- assets/script/w11.js
+++ assets/script/w11.js
@@ -1,9 +1,22 @@
-const priceK = document.getElementById('price_k');
-const priseC = document.getElementById('price_c');
-const priceS = document.getElementById('price_s');
-const priceB = document.getElementById ('price_b');
+const priceOne = document.getElementById('price_k').innerHTML = 234;
+const priseTwo = document.getElementById('price_c').innerHTML = 420;
+const priceThree = document.getElementById('price_s').innerHTML = 204;
+const priceFour = document.getElementById ('price_b').innerHTML= 34;
+
 const totalPrice = document.getElementById('totalPrice');
 const btn = document.getElementById('btn');
 
+const priceOneNumber = +priceOne;
+const priseTwoNumber = +priseTwo;
+const priceThreeNumber = +priceThree;
+const priceFourNumber = +priceFour;
 
+const totalPriceNumber = priceOneNumber + priseTwoNumber + priceThreeNumber +priceFourNumber ;
+totalPrice.textContent = totalPriceNumber + ' руб.';
 
+ function discount(){
+  const dis = totalPriceNumber - (totalPriceNumber * 0.2);
+  totalPrice.textContent = dis + ' руб.';
+ }
+
+ btn.addEventListener('click', discount);
